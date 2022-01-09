dc86f3aab02ef98c623eb131741736dd7d46ea24
Jeriefk <97401277+Jeriefk@users.noreply.github.com>
2022-01-09 22:44:16 +0530

    Update 1_Data Modelling_ETL_pipeline.py

--- 1_Data Modelling_ETL_pipeline.py
+++ 1_Data Modelling_ETL_pipeline.py
@@ -1,18 +1,18 @@
 import psycopg2
 import pandas as pd
-conn = psycopg2.connect(dbname="dbname", user="postgres", port=5432)
-curs = conn.cursor()
+conn = psycopg2.connect(dbname="order_db", user="postgres", port=5432)
+cursor = conn.cursor()
 
 def overall_report_monthly():
-    overall_monthly_report = "select * from monthly_rpt;"
-    curs.execute(overall_monthly_report)
-    overall_monthly_report_var = curs.fetchall()
-    return overall_monthly_report_var
+    overall_monthly_rpt = "select * from monthly_rpt;"
+    cursor.execute(overall_monthly_rpt)
+    overall_monthly_rpt_var = cursor.fetchall()
+    return overall_monthly_rpt_var
 def overall_status_category():
-    rpt_status_category="select * from monthly_status_category_rpt;"
-    curs.execute(rpt_status_category)
-    rpt_status_category_var = curs.fetchall()
-    return rpt_status_category_var
+    status_category_rpt="select * from monthly_status_category_rpt;"
+    cursor.execute(status_category_rpt)
+    status_category_rpt_var = cursor.fetchall()
+    return status_category_rpt_var
 if __name__ == "__main__":
     overall_report_df=pd.DataFrame(overall_report_monthly())
     overall_status_category_df=pd.DataFrame(overall_status_category())
